47df7d7ad1e7ef64afd180d41a84f0be7f884f71
lhourquin <lhourquin@gmail.com>
2024-08-12 19:01:44 +0200

    Week 1: Set problem mario level 1, done

--- Week-1/Set-Problem-1/mario.c
+++ Week-1/Set-Problem-1/mario.c
@@ -8,27 +8,22 @@ int main(void){
   nb_of_block = get_int("Size ?");
   } while( nb_of_block < 1);
 
-  int tmp = nb_of_block;
+  int length = nb_of_block;
 
-  for (int i = 0; i < tmp; i++) {
-    for (int j = 0; j < tmp; j++) {
-      if(j == (nb_of_block -1)){
-        if(tmp == nb_of_block){
-          nb_of_block--;
+  for (int i = 0; i < length; i++) {
+    for (int j = 1; j <= nb_of_block; j++) {
+      if(j == nb_of_block){
+        int tmp = nb_of_block;
+        while(tmp != length){
           printf("#");
-        }else{
-          int new_tmp = nb_of_block;
-          do{
-            printf("#");
-            new_tmp++;
-          }while(new_tmp != tmp);
-          nb_of_block--;
+          tmp++;
         }
+        nb_of_block--;
       }else{
         printf(" ");
       }
     } 
-    printf("\n");
+    printf("#\n");
   }
   return 0;
 }
