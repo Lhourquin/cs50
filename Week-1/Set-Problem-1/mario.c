63541468f9830a950b189bebfc1d0acf2e6512e0
lhourquin <lhourquin@gmail.com>
2024-08-13 02:42:16 +0200

    Week 1: Set problem mario level 1, refactor

--- Week-1/Set-Problem-1/mario.c
+++ Week-1/Set-Problem-1/mario.c
@@ -2,29 +2,32 @@
 #include <cs50.h>
 
 int main(void){
-  int nb_of_block = 0;
 
+  int blocks = 0;
+  // ask for the number of block user want
   do{
-  nb_of_block = get_int("Size ?");
-  } while( nb_of_block < 1);
+    blocks = get_int("Size ?");
+  } while( blocks< 1);
 
-  int length = nb_of_block;
+  //variable to get the lenght of block using in first loop iteration
+  int blocks_to_add = blocks;
 
-  for (int i = 0; i < length; i++) {
-    for (int j = 1; j <= nb_of_block; j++) {
-      if(j == nb_of_block){
-        int tmp = nb_of_block;
-        while(tmp != length){
+  //first loop to create the column
+  for (int i = 0; i < blocks; i++) {
+    //second loop to create the row with space and # symbols
+    for (int j = 1; j <= blocks_to_add; j++) {//second loop with nb_of_block because we decrease the number at each iteration
+      if(j != blocks_to_add){
+        printf(" ");
+      }else{
+        int tmp = blocks_to_add - 1;// tempory variable to store the actual number of block
+        while(tmp != blocks){//length doesn't change, it is always at the same length until the end.
           printf("#");
           tmp++;
         }
-        nb_of_block--;
-      }else{
-        printf(" ");
       }
     } 
-    printf("#\n");
+    printf("\n");
+    blocks_to_add--;
   }
   return 0;
 }
-
