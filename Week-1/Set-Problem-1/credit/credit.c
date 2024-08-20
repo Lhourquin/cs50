369d51234b336c0423232254166c510521742bee
lhourquin <lhourquin@gmail.com>
2024-08-20 17:27:34 +0200

    Week 1: print only second to last digit

--- Week-1/Set-Problem-1/credit/credit.c
+++ Week-1/Set-Problem-1/credit/credit.c
@@ -27,7 +27,7 @@ int main(void)
 int get_length(long number){
 
   int length = 0;
-  
+
   while (number != 0 ) {
     number = number / 10; 
     length++;
@@ -49,17 +49,27 @@ long check_sum(long number, int length){
    get only one number at each time  
    maybe I need to recalculate the actual length of the number to have the only one 
    and make operation of multpilcation with it.
-   
-   step 1: make a do while loop to decrease each time the length
-   step 2: store temporary the number popped to multpliate it by 2
-   step 3: store the result of multiplication to make addition later
+
+   step 1: print one number at each iteration ✅
+      make a modulo of number
+      divide the number by ten to pop the last number
+      check ifthe number is od or not
+        if the number is od, make operation
+        otherwise, not.
+
+   error 1: make a loop and increment by two is useless. We don't throught the number like an array with index (here, the number don't have index.)
+
    * */
-  long dividor = 1;
-  long number_splitted = 0;
+
+  long mod = 0;
+  long tmp_number = number;
   for (int i = 0; i < length; i++) {
-    dividor = dividor * 10;
-    number_splitted = number / dividor; 
-    printf("%ld -> ", number_splitted);
+    mod = tmp_number % 10;
+    tmp_number = tmp_number / 10;
+    if ((i % 2) == 1) {
+      printf("%ld -> ", mod);
+    } 
   }
-  return number_splitted;
+
+  return mod;
 }
