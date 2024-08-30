f072ef33130f595244bb7991884e78b3d5217a44
lhourquin <lhourquin@gmail.com>
2024-08-31 00:44:52 +0200

    Week 2, Set-Problem Caesar, step two, text formula to cipher char

--- Week-2/Set-Problem-2/caesar/caesar.c
+++ Week-2/Set-Problem-2/caesar/caesar.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include <cs50.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+string encrypt_plaintext(string plaintext, int key);
+
 int main(int argc, char *argv[])
 {
   //get argument of the command line to get the key to encrypt
@@ -19,11 +23,38 @@ int main(int argc, char *argv[])
       printf("Usage: %s key\n", argv[0]);
       return 1;
     }else {
+      //array of ascii decimal value
+      //make operation with modulo to get cipher char
       printf("key: %d\n", key );
+      string plaintext = get_string("plaintext: ");
+      string encrypted_text = encrypt_plaintext(plaintext, key);
+      printf("ciphertext: %s\n", encrypted_text);
     }
   } 
   
 
     
   return 0;
+}
+
+string encrypt_plaintext(string plaintext, int key){
+  string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  int length = strlen(alphabet);
+
+  string encrypted_text[length];
+
+  int i = 0;
+  while (plaintext[i] != '\0') {
+    printf("plaintext[i]: %c\n", plaintext[i]);
+    for (int j = 0; j < length;j++) {
+      if(toupper(plaintext[i]) == alphabet[j]){
+        int cipher_char = (j + key) % 26;
+        plaintext[i] = tolower(alphabet[cipher_char]);
+        break;
+      }
+    }
+    i++;
+  }
+  return plaintext;
 }
