7700edf639e77c3adb7cfb9f6eb95e6540a7363c
lhourquin <lhourquin@gmail.com>
2024-10-08 10:41:05 +0200

    Week 5: practice, linked list with linear search

--- Week-5/list.c
+++ Week-5/list.c
@@ -1,49 +1,49 @@
+// Prepends numbers to a linked list, using while loop to print 
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+typedef struct node 
 {
-    //List of size 3
-    int *list = malloc(3 * sizeof(int));
-    if(list == NULL)
-    {
-        return 1;
+    int number;
+    struct node *next;
+} node;
+
+int main(int argc, char *argv[])
+{
+    //Memory for numbers
+    node *list = NULL;
+
+    //for each command lune argument
+    for (int i = 1; i < argc; i++) {
+        //convert argument to int
+        int number = atoi(argv[i]);
+        
+        //allocate node for number
+        node *n = malloc(sizeof(node));
+        if(n == NULL){
+            return 1;
+        }
+        n->number = number;
+        n->next = NULL;
+
+        //Prepend node to list
+        n->next = list;
+        list = n;
     }
 
-    // Initialize list with numbers
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    // List of size 4
-    int *tmp = malloc(4 * sizeof(int));
-    if (tmp == NULL) {
-        free(list);
-        return 1;
+    //print numbers
+    node *ptr = list;
+    while (ptr != NULL) {
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
     }
 
-    // Copy list of size 3 into list of size 4
-    for (int i = 0 ; i < 3; i++)
-    {
-        tmp[i] = list[i];
+    ptr = list;
+    while (ptr != NULL) {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
     }
-
-    //Add number to list of size 4
-    tmp[3] = 4;
-
-    // Free list of size 3
-    free(list);
-
-    //Remember list of size 4
-    list = tmp;
-
-    //Print list
-    for (int i =0; i < 4; i++) {
-        printf("%i\n", list[i]);
-    }
-
-    // Free list
-    free(list);
-    return 0;
 }
 
